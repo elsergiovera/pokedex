4fd4d043be06d869e67817297163176eef4f2c20
Sergio Vera <elsergio@gmail.com>
2023-05-07 00:18:02 -0400

    orbit calibrated

--- src/components/pokedex.js
+++ src/components/pokedex.js
@@ -1,75 +1,89 @@
 import React, { useState, useEffect, useRef } from "react";
 import Image from 'next/image'
-
-import { Canvas, useLoader } from "@react-three/fiber";
-import { Merged, OrbitControls } from '@react-three/drei'
-// import SpriteScene from "./components/sprites"
-import { DoubleSide, TextureLoader, MeshStandardMaterial, FrontSide, Matrix4 } from "three";
-
+// import SpriteScene from "./components/sprites";
+import { Canvas, useLoader, useFrame } from "@react-three/fiber";
+import { OrbitControls } from '@react-three/drei';
+import { TextureLoader } from "three";
 import { listPokemon, getPokemonInfoByName, getPokemonDescription } from "../data/pokeapi";
 import styles from "@/styles/Pokedex.module.css";
-import { GeometryUtils } from "three-stdlib";
 
 const Pokedex = () => {
   // variables.
   const pkmnSearchBarRef = useRef();
   const [pkmnList, setPkmnList] = useState([]);
-  const [selectedPkmnList, setSelectedPkmnList] = useState("");
-  const [selectedPkmnInfo, setSelectedPkmnInfo] = useState("");
-  const [selectedPkmnDescription, setPkmnDescription] = useState("");
-
+  const [selPkmn, setSelPkmn] = useState("");
+  const [pkmnData, setPkmnData] = useState("");
+  const [pkmnDataDescription, setPkmnDataDescription] = useState("");
+  const [srcFrontSprite, setSrcFrontSprite] = useState("");
   
-  const [srcFrontSprite, setSrcFrontSprite] = useState("no_data.png");
-  // const [srcFrontSprite, setSrcFrontSprite] = useState("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/125.png");
-  // const [srcBackSprite, setSrcBackSprite] = useState("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/125.png");
-  const SpriteScene = () => {
-    const frontSprite = useLoader(TextureLoader, srcFrontSprite);
-    // const backSprite = useLoader(TextureLoader, srcBackSprite);
+  // const cameraRef = useRef();
+  // const controlsRef = useRef();
+  // const handleMouseMove = (event) => {
+  //   const controls = controlsRef.current;
+  //   const canvas = event.target;
 
-    return (
-      <mesh>
-        <directionalLight position={[0, 0, 5]} />
-        <planeGeometry args={[1.5, 1.5, 2, 2]} />
-        <meshStandardMaterial map={frontSprite} />
-        {/* 
-        <planeGeometry id={"back"} args={[1.5, 1.5, 2, 2]} applyMatrix4={makeRotationY(Math.PI)}/>
-        <meshStandardMaterial map={backSprite} /> */}
-      </mesh>
-    );
-  };
+  //   if (controls && canvas) {
+  //     const { clientWidth, clientHeight } = canvas;
+  //     const { movementX, movementY } = event;
 
+  //     controls.target.x -= movementX / clientWidth;
+  //     controls.target.y += movementY / clientHeight;
+  //   }
+  // };
 
   // functions
+  const SpriteScene = () => {
+    if(srcFrontSprite == "")
+      return (
+        <></>
+      )
+    else {
+      return (
+        <mesh>
+          <directionalLight position={[0, 0, 5]} />
+          <planeGeometry args={[1.4, 1.4, 2, 2]} />
+          <meshStandardMaterial map={useLoader(TextureLoader, srcFrontSprite)} transparent={true} />
+        </mesh>
+      );
+    }
+    
+    // useFrame(() => {
+    //   const controls = controlsRef.current;
+    //   const camera = cameraRef.current;
+  
+    //   if (controls && camera) {
+    //     camera.position.x = controls.target.x + Math.sin(Date.now() / 2000) * 2;
+    //     camera.position.y = controls.target.y + Math.sin(Date.now() / 2000) * 2;
+    //     camera.lookAt(controls.target);
+    //   }
+    // });
+  };
   const PokemonInfo = () => {
     let info = "";
 
-    if(selectedPkmnInfo != "") {
-      //set Sprite.
-      // setSrcSprite(selectedPkmnInfo.sprites.other["official-artwork"].front_default);
-
+    if(pkmnData != "") {
       // set Info.
       info = 
-      "<strong>Height:</strong> " + selectedPkmnInfo.height * 10 + " cms.<br>" +
-      "<strong>Weight:</strong> " + selectedPkmnInfo.weight / 10 + " kgs.<br>" +
+      "<strong>Height:</strong> " + pkmnData.height * 10 + " cms.<br>" +
+      "<strong>Weight:</strong> " + pkmnData.weight / 10 + " kgs.<br>" +
       "<strong>Types:</strong> ";
-      selectedPkmnInfo.types.map((item, index) => (
-        info = info + item.type.name + (selectedPkmnInfo.types.length-1 === index ? "<br>" : ", ")
+      pkmnData.types.map((item, index) => (
+        info = info + item.type.name + (pkmnData.types.length-1 === index ? "<br>" : ", ")
       ))
-      info = info + "<strong>Description:</strong> " + selectedPkmnDescription;
+      info = info + "<strong>Description:</strong> " + pkmnDataDescription;
     }
     else {
-      // setSrcSprite("/no_data.png");
       info = "<strong>No data</strong>";
     }
 
     return (
       <div dangerouslySetInnerHTML={{ __html: info }} />
     );
-  }
+  };
 
-  // handlers for Search Bar and Select.
+  // handlers for Search Bar, Button and Select.
   const handlerSearchSelect = (event) => {
-    setSelectedPkmnList(event.target.value);
+    setSelPkmn(event.target.value);
     pkmnSearchBarRef.current.value = "";
   };
   const handlerOnKeyDownSearchBar = (event) => {
@@ -77,21 +91,24 @@ const Pokedex = () => {
       handlerSearchButton();
       pkmnSearchBarRef.current.value = event.target.value;
     }
-  }
-  // hanlder for Search Button.
+  };
   const handlerSearchButton = () => {
     const getPkmnInfo = async () => {
       if (pkmnSearchBarRef.current.value != "") {
         const info = await getPokemonInfoByName(pkmnSearchBarRef.current.value);       
-        setSelectedPkmnInfo(info);
+        setPkmnData(info);
 
-        const description = await getPokemonDescription(pkmnSearchBarRef.current.value); 
-        setPkmnDescription(description);
+        if(info != "") {
+          const description = await getPokemonDescription(pkmnSearchBarRef.current.value); 
+          setPkmnDataDescription(description);
+        }
+        else
+          setPkmnDataDescription("");
 
-        setSelectedPkmnList(pkmnSearchBarRef.current.value);
+        setSelPkmn(pkmnSearchBarRef.current.value);
       }
       else
-        setSelectedPkmnInfo("");
+        setPkmnData("");
     }
     getPkmnInfo();
   };
@@ -107,30 +124,34 @@ const Pokedex = () => {
     }
     getPkmnList();
   }, []);
-
   // Pokémon list hook.
   useEffect(() => {
     const getPkmnInfo = async () => {
-      if (selectedPkmnList != "") {
-        const info = await getPokemonInfoByName(selectedPkmnList);       
-        setSelectedPkmnInfo(info);
+      if (selPkmn != "") {
+        const info = await getPokemonInfoByName(selPkmn);       
+        setPkmnData(info);
 
-        const description = await getPokemonDescription(selectedPkmnList);
-        setPkmnDescription(description);
+        const description = await getPokemonDescription(selPkmn);
+        setPkmnDataDescription(description);
       }
+      else {
+        setSelPkmn("");
+        setPkmnData("");
+        setPkmnDataDescription("");
+      }
+
     }
     getPkmnInfo();
-  }, [selectedPkmnList]);
-
+  }, [selPkmn]);
   // selected Pokémon hook.
   useEffect(() => {
-    if (selectedPkmnInfo !== "")
-      setSrcFrontSprite(selectedPkmnInfo.sprites.other["official-artwork"].front_default)
+    if (pkmnData !== "")
+      setSrcFrontSprite(pkmnData.sprites.other["official-artwork"].front_default)
     else
-      setSrcFrontSprite("no_data.png");
+      setSrcFrontSprite("");
     PokemonInfo();
 
-  }, [selectedPkmnInfo]);
+  }, [pkmnData]);
 
   return (
     <div className={styles.pokedex}>
@@ -138,7 +159,8 @@ const Pokedex = () => {
         <div className={styles.screen}>
           <div className={styles.screenCanvas}>
             <Canvas
-              style={{ border: "8px solid #2d2b2c", backgroundColor: "#4fa95f" }}
+              // onMouseMove={handleMouseMove}
+              style={{ border: "10px solid #2d2b2c", backgroundColor: "#4fa95f" }}
               shadows="soft"
               camera={{
                 fov: 75,
@@ -147,18 +169,20 @@ const Pokedex = () => {
                 position: [0, 0, 1]
               }}
             >
+              {/* <perspectiveCamera
+                ref={cameraRef}
+                position={[0, 0, 10]} /> */}
               <SpriteScene />
               <OrbitControls
+                // ref={controlsRef}
                 enableDamping={false}
-                // dampingFactor={0.1}
-                // enablePan={false}
                 enableRotate={true}
                 enableZoom={false}
                 rotateSpeed={0.5}
-                // minAzimuthAngle={-Math.PI / 4}
-                // maxAzimuthAngle={Math.PI / 4}
-                minPolarAngle={Math.PI / 2}
-                maxPolarAngle={Math.PI / 2}
+                minAzimuthAngle={-Math.PI / 20}
+                maxAzimuthAngle={Math.PI / 20}
+                minPolarAngle={Math.PI / 2 - 0.1}
+                maxPolarAngle={Math.PI / 2 + 0.1}
               />
             </Canvas>
           </div>
@@ -167,7 +191,7 @@ const Pokedex = () => {
             id={"searchSelect"}
             className={styles.select}
             style={{textTransform: "capitalize"}}
-            value={selectedPkmnList}
+            value={selPkmn}
             onChange={handlerSearchSelect}>
               <option key={0} value={""}>--</option>
               {pkmnList.length > 0 ? (
@@ -199,7 +223,7 @@ const Pokedex = () => {
         {/* <datalist id={"PkmnList"}>
             {(pkmnList.length > 0) ? (
               pkmnList.map((item, index) => (
-                <option value={item.name}>{`#${index+1} - ${item.name}`}</option>
+                <option key={index} value={item.name}>{`#${index+1} - ${item.name}`}</option>
               ))
             ) : (<></>)}
         </datalist> */}
--- src/components/sprites.js
+++ src/components/sprites.js
@@ -1,15 +1,42 @@
+import React, { useState, useEffect } from "react";
 import { TextureLoader } from "three";
-import { useLoader } from "@react-three/fiber";
+import { Canvas, useLoader } from "@react-three/fiber";
+import { OrbitControls } from '@react-three/drei'
 
-const SpriteScene = (sprite) => {
-  const sprite = useLoader(TextureLoader, "../public/no_data.png");
+const SpriteScene = (props) => {
+  // const [srcFrontSprite, setSrcFrontSprite] = useState(() => {useLoader(TextureLoader, props.frontSprite)});
+
+  useEffect(() => {
+  }, [props.frontSprite])
 
   return (
-    <mesh scale={1.5}>
-      <directionalLight position={[0, 0, 5]} />
-      <planeGeometry  />
-      <meshStandardMaterial map={sprite} transparent/>
-    </mesh>
+    <>
+    <Canvas
+      style={{ border: "8px solid #2d2b2c", backgroundColor: "#4fa95f" }}
+      shadows="soft"
+      camera={{
+        fov: 75,
+        near: 0.1,
+        far: 1000,
+        position: [0, 0, 1]
+      }}
+    >
+      <OrbitControls
+        enablePan={false}
+        enableRotate={true}
+        enableZoom={false}
+        rotateSpeed={0.5}
+
+        minPolarAngle={Math.PI / 2}
+        maxPolarAngle={Math.PI / 2}
+      />
+      <mesh>
+        <directionalLight position={[0, 0, 5]} />
+        <planeGeometry args={[1.5, 1.5, 2, 2]} />
+        <meshStandardMaterial map={frontSprite} />
+      </mesh>
+    </Canvas>
+    </>
   );
 };
 
