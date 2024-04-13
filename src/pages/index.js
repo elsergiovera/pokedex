4c80884abe3d0a173d10155436bb7734e91c4974
elsergio <elsergio@gmail.com>
2024-04-13 16:46:15 -0400

    fixed url

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
       <Head>
         <title>Pokedex</title>
         <meta name="description" content="Deva's Pokedex" />
-        <link rel="icon" href="pokedex/favicon.ico" />
+        <link rel="icon" href="./favicon.ico" />
       </Head>
       <Pokedex />
     </>
